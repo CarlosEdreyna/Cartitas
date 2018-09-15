8dd5d31303ecad3c4a0389ffd89c966bd08f70ef
CarlosEdreyna <42684019+CarlosEdreyna@users.noreply.github.com>
2018-09-14 19:18:11 -0500

    Cartas upload

---
+++ Cartas/Assets/Scripts/Cartit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cartit : MonoBehaviour {
+
+    public Material[] randomMaterials;
+
+    public GameObject[] card;
+
+    // Use this for initialization
+    void Start () {
+
+        foreach (GameObject CardFBX in card)
+        {
+            CardFBX.GetComponent<Renderer>().material = randomMaterials[Random.Range(0, randomMaterials.Length)];
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Cartas/Assets/Scripts/Posicion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Posicion : MonoBehaviour {
+
+    public Material[] randomMaterials;
+
+    public GameObject[] card;
+
+
+    // Use this for initialization
+    void Start () {
+
+        /*foreach (Material materials in randomMaterials)
+        {
+            foreach (GameObject CardFBX in card)
+            {
+                CardFBX.GetComponent<Renderer>().material = randomMaterials[Random.Range(0, randomMaterials.Length)];
+            }
+        }*/
+        
+        int[] indMat = new int[randomMaterials.Length];
+        
+        for (int i = 0; i < randomMaterials.Length; i++)
+        {
+            indMat[i] = 0; 
+        }
+        foreach (GameObject CardFBX in card)
+        { 
+            int num = Random.Range(0, randomMaterials.Length);
+            
+            while (indMat[num] != 0)
+            {
+                num++;
+                if (num >= randomMaterials.Length)
+                {
+                    num = 0;
+                }
+            }
+            indMat[num] = 1;
+            CardFBX.GetComponent<Renderer>().material = randomMaterials[num];
+        }
+    }
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
